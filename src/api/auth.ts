a6413e512ee0e3f13aae86acc958fac85d53037c
GangHyeokLee <rkdgur5381@gmail.com>
2025-05-06 21:37:49 +0900

    Feat: saving access token

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,14 +1,22 @@
 import apiClient from "@/api/apiClient";
 
 export const login = async (id: string, password: string) => {
-    const response = await apiClient.post("/auth/login", {
+    const response: { data: { code: number, access_token: string } } = await apiClient.post("/auth/login", {
         id, password
     });
-    return response.data;
+
+    if (response.data.code === 200) {
+        localStorage.setItem("accessToken", response.data.access_token);
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
 export const logout = async () => {
     const response = await apiClient.post("/auth/logout");
+    localStorage.removeItem("accessToken");
 }
 
 export const signup = async (id: string, name: string, password: string, role: string) => {
