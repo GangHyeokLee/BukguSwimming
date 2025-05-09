9c8be4897dd0d4d1e52efde8485ddf51707646e4
GangHyeokLee <rkdgur5381@gmail.com>
2025-05-09 20:13:34 +0900

    Chore: Lane Types

--- src/types/lanes.ts
+++ src/types/lanes.ts
@@ -7,3 +7,16 @@ export interface LaneListType {
   swimming_name: string;
   team: string;
 }
+
+export interface LaneDetailType {
+  id: number;
+  lane: number;
+  play_num: number;
+  dq: string;
+  team: string;
+  player: string;
+  swimming_name: string;
+  record: string;
+  prev: number | null;
+  next: number | null;
+}
