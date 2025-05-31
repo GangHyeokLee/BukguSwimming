e265294ad5d12c8dc3af15dbc00eacc1d4ffa070
GangHyeokLee <rkdgur5381@gmail.com>
2025-05-31 12:49:49 +0900

    Feat: admin access control

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,16 @@
+import { parseJwt } from "@/utils/parseJwt";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import React from "react"
+
+export default async function AdminLayout({children}:{children: React.PropsWithChildren}) {
+
+  const cookieStore = await cookies();
+  const token = cookieStore.get("accessToken")?.value;
+
+  if(!token || ![0, 7, 8].includes(parseJwt(token))){
+    redirect("/login");
+  }
+
+  return <>{children}</>;
+}
