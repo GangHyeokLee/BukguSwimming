91411d8f045226daae2adfc4b1c8d30b3499b3cd
GangHyeokLee <rkdgur5381@gmail.com>
2025-06-28 15:17:35 +0900

    Feat: update PrintCertificate component to fetch and display certificate data

--- src/app/admin/certificate/print/page.tsx
+++ src/app/admin/certificate/print/page.tsx
@@ -1,69 +1,97 @@
 'use client';
 
+import { printCertificate } from "@/api/admin/client";
 import dojang from "@/assets/도장.png";
+import { CertificateResponse } from "@/types/cert";
 import { useSearchParams } from 'next/navigation'
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function PrintCertificate() {
   const searchParams = useSearchParams();
-  const player = searchParams?.get('player')
-  const record = searchParams?.get('record')
-  const rank = searchParams?.get('rank')
-  const swimming_name = searchParams?.get('swimming_name')
-  const team = searchParams?.get('team')
+  const [data, setData] = useState<CertificateResponse | null>(null);
+  const id = searchParams?.get('id')
 
   useEffect(() => {
-    const handleAfterPrint = () => {
-      window.close();
-    };
+    // const handleAfterPrint = () => {
+    //   window.close();
+    // };
 
-    window.onafterprint = handleAfterPrint;
+    (async () => {
+      if (id) {
+        const res = await printCertificate(parseInt(id)) as CertificateResponse;
+        setData(res);
+      }
+    })();
 
-    // print 전에 렌더링 안정화 위해 약간 지연
-    const timeoutId = setTimeout(() => {
-      window.print();
-    }, 1000);
+    // window.onafterprint = handleAfterPrint;
 
-    // 혹시 afterprint가 작동안할 경우 대비 예비 닫기
-    const forceCloseTimeoutId = setTimeout(() => {
-      window.close();
-    }, 3000);
+    // // print 전에 렌더링 안정화 위해 약간 지연
+    // const timeoutId = setTimeout(() => {
+    //   window.print();
+    // }, 1000);
 
-    return () => {
-      clearTimeout(timeoutId);
-      clearTimeout(forceCloseTimeoutId);
-      window.onafterprint = null;
-    };
+    // // 혹시 afterprint가 작동안할 경우 대비 예비 닫기
+    // const forceCloseTimeoutId = setTimeout(() => {
+    //   window.close();
+    // }, 3000);
+
+    // return () => {
+    //   clearTimeout(timeoutId);
+    //   clearTimeout(forceCloseTimeoutId);
+    //   window.onafterprint = null;
+    // };
   }, []);
 
-  return (
+  return data && (
     <div
       id="certificate-print"
       className="w-[595px] h-[842px] bg-white px-[80px] py-[60px] text-center flex flex-col justify-between font-gulim text-black mx-auto print:w-[595px] print:h-[842px]"
     >
       {/* 기존 상장 코드 그대로 복붙 */}
-      <div className="text-left text-base mb-2">제2024-101호</div>
+      <div className="text-left text-base mb-2">제{data.cert_data.cert_num}호</div>
       <h1 className="text-6xl font-bold tracking-wider mb-6">상&nbsp;&nbsp;&nbsp;&nbsp;장</h1>
-      <div className="text-left text-xl leading-10 grid grid-cols-2 mb-4">
-        <p>종목: {swimming_name}</p>
-        <p>순위: {rank}위</p>
-        <p>종별: {swimming_name}</p>
-        <p>소속: {team}</p>
-        <p>기록: {record}</p>
-        <p>성명: {player}</p>
+      <div className="text-left text-2xl leading-10 grid grid-cols-2 mb-4 whitespace-nowrap">
+        <p>종목: {data.swimming_data.swim_type}</p>
+        <p>순위: {data.swimming_data.rank}위</p>
+        <p>종별: {data.swimming_data.depart}&nbsp;{data.swimming_data.gender}&nbsp;{data.swimming_data.group}</p>
+        <p>소속: {data.swimming_data.team}</p>
+        <p>
+          기록: {(() => {
+            const ms = data.swimming_data.record;
+            const min = Math.floor(ms / 60000);
+            const sec = Math.floor((ms % 60000) / 1000) % 60;
+            const msRemain = ms % 1000;
+            return `${min}'${String(sec).padStart(2, '0')}"${String(msRemain).padStart(3, '0')}`;
+          })()}
+        </p>
+        <p>성명: {data.swimming_data.player}</p>
       </div>
-      <div className="text-lg leading-9 whitespace-pre-line mt-6">
+      <div className="text-2xl leading-9 whitespace-pre-line mt-6">
         위 사람은 대구광역시 북구체육회에서 주최하는<br />
-        제 {swimming_name}에서 위와 같이 우수한 성적으로<br />
+        <strong>{data.cert_data.cert_name}</strong>에서 위와 같이 우수한 성적으로
         입상하였기에 이 상장을 수여합니다.
       </div>
       <div className="text-xl leading-8 mt-12">
-        <p className="mb-2">2025년 5월 31일</p>
-        <p className="relative z-10">대구광역시북구수영연맹</p>
+        <p className="mb-2 text-2xl">{
+          (() => {
+            const d = new Date();
+            return `${d.getFullYear()}년 ${d.getMonth() + 1}월 ${d.getDate()}일`;
+          })()
+        }</p>
+        <br />
+        <br />
+        <p className="relative text-2xl font-bold z-10">대구광역시북구수영연맹</p>
         <div className="mt-2 relative inline-block">
           <img src={dojang.src} alt="도장" className="absolute right-[-80px] top-[-40px] z-0"
             style={{ width: '103px', height: '97.2px', opacity: 0.6 }} />
-          <p className="inline-block relative z-10">회장&nbsp;&nbsp;&nbsp;&nbsp;김&nbsp;&nbsp;&nbsp;기&nbsp;&nbsp;&nbsp;출</p>
+          <p className="inline-block text-2xl relative z-10">
+            회장&nbsp;&nbsp;&nbsp;&nbsp;
+            <span className="text-3xl font-bold">
+              {data.cert_data.president.split('').map((ch, i) => (
+                <span key={i}>{ch}{i < data.cert_data.president.length - 1 ? '\u00A0\u00A0\u00A0' : ''}</span>
+              ))}
+            </span>
+          </p>
         </div>
       </div>
     </div>
